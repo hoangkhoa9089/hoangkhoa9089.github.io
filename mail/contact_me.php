81f4d1b87c63e9610b6c9886647cf4eb5d121473
KHOAHD\Administrator <khoahd@allexceed.co.jp>
2017-07-18 11:27:51 +0700

    # contact tesst

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,39 +13,35 @@ $name = $_GET['name'];
 $email_address = $_GET['email'];
 $message = $_GET['message'];
 
-var_export($name, true);
-var_export($email_address, true);
-var_export($message, true);
+$to = 'hoangkhoa9089@gmail.com';
+$email_subject = "Github Contact Form:  $name";
+$email_body = "You have received a new message from your github contact form.<br/>
+Here are the details:<br/>
+Name: $name<br/>
+Email: $email_address<br/>
+Message:<br/><p style='padding: 10px;'>$message</p>";
 
-// $to = 'hoangkhoa9089@gmail.com';
-// $email_subject = "Github Contact Form:  $name";
-// $email_body = "You have received a new message from your github contact form.<br/>
-// Here are the details:<br/>
-// Name: $name<br/>
-// Email: $email_address<br/>
-// Message:<br/><p style='padding: 10px;'>$message</p>";
+require 'class.smtp.php';
+require 'class.phpmailer.php';
 
-// require 'class.smtp.php';
-// require 'class.phpmailer.php';
+$mail = new PHPMailer;
 
-// $mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'khoahd.mailer@gmail.com';
+$mail->Password = 'A@b12345';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
 
-// $mail->isSMTP();
-// $mail->Host = 'smtp.gmail.com';
-// $mail->SMTPAuth = true;
-// $mail->Username = 'khoahd.mailer@gmail.com';
-// $mail->Password = 'A@b12345';
-// $mail->SMTPSecure = 'ssl';
-// $mail->Port = 465;
+$mail->setFrom('khoahd.mailer@gmail.com', 'Github Mailer');
+$mail->addAddress($to);
+$mail->addReplyTo($email_address);
+$mail->isHTML(true);
 
-// $mail->setFrom('khoahd.mailer@gmail.com', 'Github Mailer');
-// $mail->addAddress($to);
-// $mail->addReplyTo($email_address);
-// $mail->isHTML(true);
-
-// $mail->Subject = $email_subject;
-// $mail->Body    = $email_body;
-// $mail->AltBody = $email_body; //for none html client
+$mail->Subject = $email_subject;
+$mail->Body    = $email_body;
+$mail->AltBody = $email_body; //for none html client
 
 if(!$mail->send()) {
     return false;
